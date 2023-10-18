54662d36f8749e2a3cc24593cc56092ee1873841
kinntarou204 <220002kr@yse-c.net>
2023-10-18 09:05:27 +0900

    update

--- express_sample/routes.js
+++ express_sample/routes.js
@@ -3,6 +3,33 @@ const express = require('express')
 // Routerオブジェクトを生成
 const router = express.Router()
 
+// GETリクエストの処理
+router.get('/', (req, res) => {
+    // リクエストの処理
+    console.log(req.body)
+    console.log(req.url)
+    console.log(req.query)
+
+    // レスポンスの処理
+    res.send('Hello!!!!!!')
+})
+
+router.get('/profile', (req, res) => {
+    res.send('プロフィール')
+})
+
+router.post('/auth', (req, res) => {
+    var loginName = req.body.login_name
+    var password = req.body.password
+    console.log(loginName, password)
+
+    var message = "ログイン失敗"
+    if (loginName == process.env.LOGIN_NAME
+        && password == process.env.PASSWORD) {
+            message = "ログイン成功"
+    }
+    res.send(message)
+})
 
 
 // モジュール化
--- express_sample/server.js
+++ express_sample/server.js
@@ -3,6 +3,7 @@ const express = require('express')
 // dotenvモジュール読み込み
 const dotenv = require('dotenv')
 
+const routes=require('./routes')
 // dotenvの設定読み込み
 dotenv.config()
 const HOST = process.env.HOST
@@ -18,33 +19,7 @@ app.use(express.static(__dirname + './public'))
 // URLエンコード
 app.use(express.urlencoded({ extended: true }))
 
-// GETリクエストの処理
-app.get('/', (req, res) => {
-    // リクエストの処理
-    console.log(req.body)
-    console.log(req.url)
-    console.log(req.query)
-
-    // レスポンスの処理
-    res.send('Hello!!!!!!')
-})
-
-app.get('/profile', (req, res) => {
-    res.send('プロフィール')
-})
-
-app.post('/auth', (req, res) => {
-    var loginName = req.body.login_name
-    var password = req.body.password
-    console.log(loginName, password)
-
-    var message = "ログイン失敗"
-    if (loginName == process.env.LOGIN_NAME
-        && password == process.env.PASSWORD) {
-            message = "ログイン成功"
-    }
-    res.send(message)
-})
+app.use(routes)
 
 //　サーバ停止: 起動中のターミナルで Ctrl + C
 // サーバ待機（Listen）
