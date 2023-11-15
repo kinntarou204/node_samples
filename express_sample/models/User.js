06517d7d4fee03244493324c4fd7f7c3e214708f
kinntarou204 <220002kr@yse-c.net>
2023-11-15 11:28:52 +0900

    update

--- express_sample/models/User.js
+++ express_sample/models/User.js
@@ -1,10 +1,22 @@
 //Modelモジュール読み込み
 const Model = require('mysql2/promise')
 const db=require('../lib/db')
-//Modelクラスを継承
+//Modelクラスを継承 
 class User extends Model {
     add=async(post)=>{
+        var sql=`INESRT INTO users SET ?;`
+        try{
+            const con= await mysql.createConnection(db.info);
+            var reult;
+            reult=con.query(sql,post);
+        }
+        catch(error){
 
+        }
+        finally{
+            con.end();
+        }
+        return reult;
     }  
     auth = (email, password) => {
         
