52fc2e6e3f52300cd3d134ecfc472a7c3e1f811e
kinntarou204 <220002kr@yse-c.net>
2023-11-15 11:12:24 +0900

    update

--- express_sample/lib/db.js
+++ express_sample/lib/db.js
@@ -1,10 +1,10 @@
-const dotenv=require('dotenv')
+const dotenv = require('dotenv')
 dotenv.config()
 
-exports.info={
-    database:process.env.DB_NAME,
-    database:process.env.DB_HOST,
-    database:process.env.DB_PORT,
-    database:process.env.DB_USER,
-    database:process.env.DB_PASSWORD,
+exports.info = {
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
 }
