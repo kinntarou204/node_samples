4b79b6b50be06073a61875b68d490b9abd405e6c
kinntarou204 <220002kr@yse-c.net>
2023-11-15 11:37:00 +0900

    update

---
+++ express_sample/test/insert.js
@@ -0,0 +1,28 @@
+//mysql2/promiseモジュール読み込み
+const mysql = require('mysql2/promise')
+const db = require('../lib/db')
+
+class User {
+    //ユーザ追加（非同期処理）
+    add = async (post) => {
+        //DB接続
+        var result;
+        try {
+            const con = await mysql.createConnection(db.info);
+            //SQL実行
+            var sql = `INSERT INTO users SET ?;`
+            result = con.query(sql, post);
+            con.end();
+        } catch (error) {
+
+        } finally {
+        }
+        return result;
+    }
+    auth = (email, password) => {
+
+    }
+}
+
+//モジュール化
+module.exports = User
